8f6946157136accaa87d18689a1a035337abe390
Kayque Goldner <kayquegoldner@gmail.com>
2025-01-01 11:27:07 -0300

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import './App.css'
-import { Header } from './sections/Header'
+import "./App.css";
+import { Header } from "./sections/Header";
+import { Hero } from "./sections/Hero";
 
 function App() {
-
   return (
     <main className="overflow-hidden">
       <Header />
+      <Hero />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
