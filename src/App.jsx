4a6b28b2c4cc6b47da7f8ad77a5081ab4b293a8f
Kayque Goldner <kayquegoldner@gmail.com>
2025-01-18 23:11:42 -0300

    download section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Download } from "./sections/Download";
 import { Faq } from "./sections/Faq";
 import { Features } from "./sections/Features";
 import { Header } from "./sections/Header";
@@ -15,6 +16,7 @@ function App() {
       <Pricing />
       <Faq />
       <Testimonials />
+      <Download />
     </main>
   );
 }
